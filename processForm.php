2cb35ca5c79a1263c1f71f2670e2bf2ab8fe6942
Perla Del Castillo (perlasamanez) <perlitadelcastillo@gmail.com>
2018-01-21 06:32:37 -0500

    Modified

--- processForm.php
+++ processForm.php
@@ -5,11 +5,29 @@ if (count($_POST) == 0) {
 }
 
 $first = strip_tags($_POST['first']);
-$expiry = strip_tags($_POST['expiry']);
+$second = strip_tags($_POST['second']);
+$third = strip_tags($_POST['third']);
+
+
+$expiry = new DateTime('04-02-2018');
+$expirynext = new DateTime('11-02-2018');
+$expirynextnext = new DateTime('28-01-2018');
+
+$today = new DateTime('now');
+
+$difference = $today->diff($expiry)->format('%R%a');
+$differencenext = $today->diff($expirynext)->format('%R%a');
+$differencenextnext = $today->diff($expirynextnext)->format('%R%a');
+
+//$difference = $expiry->diff($today)->format('%R%a');
 
 $data = json_decode(file_get_contents('https://api.walmartlabs.com/v1/items?apiKey=fhd34cqau75b2mgqxhuhjras&upc=' . $first), true);
+$datanext = json_decode(file_get_contents('https://api.walmartlabs.com/v1/items?apiKey=fhd34cqau75b2mgqxhuhjras&upc=' . $second), true);
+$datanextnext = json_decode(file_get_contents('https://api.walmartlabs.com/v1/items?apiKey=fhd34cqau75b2mgqxhuhjras&upc=' . $third), true);
 
 $items = $data['items'];
+$itemsnext = $datanext['items'];
+$itemsnextnext = $datanextnext['items'];
 
 ?>
 <!doctype html>
@@ -23,7 +41,17 @@ $items = $data['items'];
   <?php foreach ($items as $item): ?>
     <p>Name: <?= $item['name'] ?></p>
     <p>Picture:</p> <img src="<?= $item['thumbnailImage'] ?>"/>
-    <p>Expiration Date (in days): 10 days</p>
+    <p>Expired? <?=$difference < 8 ? 'You have a week to replace this item.' : sprintf("You still have this amount of days: %s - %s = %s day(s)", $expiry->format('m/d/Y'), $today->format('m/d/Y'), $difference)  ?>
+  <?php endforeach; ?>
+  <?php foreach ($itemsnext as $item): ?>
+    <p>Name: <?= $item['name'] ?></p>
+    <p>Picture:</p> <img src="<?= $item['thumbnailImage'] ?>"/>
+    <p>Expired? <?=$differencenext < 8 ? 'You have a week to replace this item.' : sprintf("You still have this amount of days: %s - %s = %s day(s)", $expirynext->format('m/d/Y'), $today->format('m/d/Y'), $differencenext)  ?>
+  <?php endforeach; ?>
+  <?php foreach ($itemsnextnext as $item): ?>
+    <p>Name: <?= $item['name'] ?></p>
+    <p>Picture:</p> <img src="<?= $item['thumbnailImage'] ?>"/>
+    <p>Expired? <?=$differencenextnext < 8 ? 'You have a week to replace this item.' : sprintf("You still have this amount of days: %s - %s = %s day(s)", $expirynextnext->format('m/d/Y'), $today->format('m/d/Y'), $differencenextnext)  ?>
   <?php endforeach; ?>
 </body>
 
